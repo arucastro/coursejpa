153b931072283a8f5dd38480c4d41dcd851033e4
sandro <sandrosilva9029@gmail.com>
2022-08-10 15:36:47 -0300

    camada de servico e registro de componente

--- src/main/java/com/sandro/coursejpa/resources/ClientResource.java
+++ src/main/java/com/sandro/coursejpa/resources/ClientResource.java
@@ -1,25 +1,36 @@
 package com.sandro.coursejpa.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sandro.coursejpa.entities.Client;
+import com.sandro.coursejpa.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	
 	@GetMapping
-	public ResponseEntity<Client> findAll(){
-		Client c = new Client(null, "Eduardo", "eduardo@mail.com", "999999999", "12345" );
-		
-		return ResponseEntity.ok().body(c);
-		
-		
-		
+	public ResponseEntity<List<Client>> findAll(){
+		List<Client> list = service.findAll();		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Client> findbyId (@PathVariable Long id){
+		Client obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/sandro/coursejpa/services/ClientService.java
@@ -0,0 +1,28 @@
+package com.sandro.coursejpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sandro.coursejpa.entities.Client;
+import com.sandro.coursejpa.repositories.ClientRepository;
+
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	public List<Client> findAll (){
+		return repository.findAll();
+	}
+	
+	
+	public Client findById (Long id) {
+		Optional<Client> obj = repository.findById(id);
+		return obj.get();
+	}
+}
