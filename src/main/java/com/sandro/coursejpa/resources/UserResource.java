c510aa1efe73cc5319f9c57c0482603f11fcb470
sandro <sandrosilva9029@gmail.com>
2022-08-10 14:01:16 -0300

    criado user e user resource

---
+++ src/main/java/com/sandro/coursejpa/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.sandro.coursejpa.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sandro.coursejpa.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(null, "Eduardo", "eduardo@mail.com", "999999999", "12345" );
+		
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
